f9e5621f336701a6d2cd97265396410ba4a299c9
NehaGajane <137098317+NehaGajane@users.noreply.github.com>
2024-01-16 19:34:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let fname = document.getElementById('fname');
+let lname = document.getElementById('lname');
+let phone = document.getElementById('phone');
+let email = document.getElementById('email');
+
+
+document.getElementById('btn').addEventListener('click', function() {
+	alert(`First Name: ${fname.value} /n
+Last Name: ${lname.value} /n	Phone Number: ${phone.value} /n
+Email ID: ${email.value}`);
+});
